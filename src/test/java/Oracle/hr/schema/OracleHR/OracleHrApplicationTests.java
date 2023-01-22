53779d709e9c8d7d2625ba8bbca5f9f9ce3fca51
rse <regimantas.seipunas@sapiens.com>
2023-01-22 22:34:12 +0200

    Initial commit

---
+++ src/test/java/Oracle/hr/schema/OracleHR/OracleHrApplicationTests.java
@@ -0,0 +1,13 @@
+package Oracle.hr.schema.OracleHR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
